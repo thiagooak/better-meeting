decb94f1457a544c28276780e56f3864c4ae3aa3
Thiago Carvalho <thiago.carvalho2@nubank.com.br>
2022-12-08 09:16:41 -0300

    Improve mobile view

--- src/better_meeting/core.cljs
+++ src/better_meeting/core.cljs
@@ -32,11 +32,8 @@
         ; these are wrong as they consider 24h days and out unit is 8h days (business days)
         map {"3" "or the amount of time it would take you to watch every episode of Breaking Bad"
              "6" "or the amount of time it would take you to watch the original Star Ward trilogy"
-             "9" "or the amount of time it would take you to watch every episode of RuPaul's Drag Race"
-             }]
-    (map t)
-    )
-  )
+             "9" "or the amount of time it would take you to watch every episode of RuPaul's Drag Race"}]
+    (map t)))
 
 (defn calc
   [attendees duration]
@@ -53,7 +50,7 @@
 
 (defn attendees-input [value]
   [:input {:type "number"
-           :class "w-16 border-2 border-indigo-500 p-2"
+           :class "w-16 border-2 border-indigo-500 p-2 mt-2 md:mt-0 rounded"
            :default-value value  :min 1
            :on-change (fn [e]
                         (let [new-value (js/parseInt (.. e -target -value))
@@ -64,7 +61,7 @@
 
 (defn duration-input [value]
   [:input {:type "number"
-           :class "w-24 border-2 border-indigo-500 p-2"
+           :class "w-24 border-2 border-indigo-500 p-2 rounded"
            :default-value value :min 0 :step 15
            :on-change (fn [e]
                         (let [new-value (js/parseInt (.. e -target -value))
@@ -78,7 +75,7 @@
 
 (defn home-page []
   (let [{:keys [attendees duration context meeting business note]} @meeting-data]
-    [:div {:class "mx-auto max-w-xl font-mono my-4"}
+    [:div {:class "mx-4 sm:mx-auto max-w-xl font-mono my-2"}
      [:h1 {:class "text-xl mb-4"} "How much time goes into a meeting?"]
      [:div "A " [duration-input duration] " minute meeting with "
       [attendees-input attendees] " people takes:"]
@@ -87,19 +84,16 @@
       [:div "+ combined meeting time"]
       [:div meeting [:span {:class "text-sm"} " minutes"]]
       [:div "+ " [:a {:class "underline" :href "https://erichorvitz.com/CHI_2007_Iqbal_Horvitz.pdf"} "context switch"] " tax "]
-      [:div context [:span {:class "text-sm"} " minutes"]]
-      ]
+      [:div context [:span {:class "text-sm"} " minutes"]]]
      [:div {:class "border-dashed border-t-2 border-gray-600 my-4"}]
      [:div {:class "grid grid-cols-2 gap-2 pl-8"}
       [:div "= total time investment"]
-      [:div {:class "text-indigo-500 font-bold"} (round business 1) [:span {:class "text-sm"}" business days"]]
+      [:div {:class "text-indigo-500 font-bold"} (round business 1) [:span {:class "text-sm"} " business days"]]
       [:div " "]
-[:div {:class "text-gray-400"} note]
-      ]
+      [:div {:class "text-gray-400"} note]]
      [:div {:class "text-gray-300 mt-12"}
-      [:a { :href "https://github.com/thiagooak/better-meeting" :target "_blank"} [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "none" :view-box "0 0 24 24" :stroke-width "1.5" :stroke "currentColor" :class "w-6 h-6"}
-                                                                                                                    [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"}]]]]
-     ]))
+      [:a {:href "https://github.com/thiagooak/better-meeting" :target "_blank"} [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "none" :view-box "0 0 24 24" :stroke-width "1.5" :stroke "currentColor" :class "w-6 h-6"}
+                                                                                  [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"}]]]]]))
 
 ;; -------------------------
 ;; Initialize app
